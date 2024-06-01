96c74e8e3e95533b8df22410f032627dbaaa930a
JanaDolejsi <jana.dolejsi.gr@gmail.com>
2024-06-01 17:04:07 +0200

    úkol č.16

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,47 +1,30 @@
+import { Joke } from '../Joke';
 import './style.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const HomePage = () => {
-  const [likesUp, setLikesUp] = useState(0);
-  const [likesDown, setLikesDown] = useState(0);
+const [joke, setJoke] = useState([])
 
-  const increaseLikesUp = () => {
-    setLikesUp(likesUp + 1);
+
+
+useEffect(() => {
+  const fetchName = async () => {
+    const response = await fetch('http://localhost:4000/api/jokes');
+    const json = await response.json();
+    setJoke(json.data);
   };
 
-  const increaseLikesDown = () => {
-    setLikesDown(likesDown + 1);
-  };
+  fetchName();
+}, []);
   
- 
+
 
   return (
     <div className="container">
-      
-      <main>
-      <div class="container">
-      <div class="joke">
-        <div class="joke__body">
-          <div class="joke__user">
-          <img class="user-avatar" src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png" />
-            <p class="user-name">Neroxx</p>
-          </div>
-          <p class="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div class="joke__likes">
-          <button id="btn-up" class="btn-like btn-like--up" onClick={increaseLikesUp}></button>
-          <span id="likes-up" class="likes-count likes-count--up">{likesUp}</span>
-          <button id="btn-down" class="btn-like btn-like--down" onClick={increaseLikesDown}></button>
-          <span id="likes-down" class="likes-count likes-count--down">{likesDown}</span>
-        </div>
-      </div>
-    </div>
-      </main>
-      
+      {joke.map((item) => (
+        <Joke key={item.id} userAvatar={item.avatar} userName={item.name} text={item.text} likes={item.likes} dislikes={item.dislikes}/> 
+      ))}
+     
     </div>
   );
 
---
+++ src/pages/Joke/index.jsx
@@ -0,0 +1,41 @@
+import './style.css';
+import React, { useState } from 'react';
+
+
+
+
+export const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {
+    const [likesUp, setLikesUp] = useState(likes);
+    const [likesDown, setLikesDown] = useState(dislikes);
+
+    const increaseLikesUp = () => {
+    setLikesUp(likesUp + 1);
+  };
+
+    const increaseLikesDown = () => {
+    setLikesDown(likesDown + 1);
+  };
+
+return (
+    
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+          <img className="user-avatar" src={userAvatar} />
+            <p className="user-name">{userName}</p>
+          </div>
+          <p className="joke__text">
+            {text}
+          </p>
+        </div>
+        <div className="joke__likes">
+          <button id="btn-up" className="btn-like btn-like--up" onClick={increaseLikesUp}></button>
+          <span id="likes-up" className="likes-count likes-count--up">{likesUp}</span>
+          <button id="btn-down" className="btn-like btn-like--down" onClick={increaseLikesDown}></button>
+          <span id="likes-down" className="likes-count likes-count--down">{likesDown}</span>
+        </div>
+      </div>
+   
+)
+
+}
